580640bf39f3be937ef564b98c002e50e265fddd
Marvin <marvin88gr@gmail.com>
2021-10-22 23:18:14 +0100

    add test files

--- test/test-init.js
+++ test/test-init.js
@@ -1,25 +1,5 @@
 const { parseEther } = ethers.utils;
 
-const PROXY_CREATION = "ProxyCreation";
-
-const getGnosisProxyInstance = async (setup) => {
-  const gnosisSafeProxyFactoryFactory = await ethers.getContractFactory(
-    "GnosisSafeProxyFactory",
-    setup.roles.prime
-  );
-  const gnosisSafeProxyFactoryInstance =
-    await gnosisSafeProxyFactoryFactory.deploy();
-
-  const proxy_tx = await gnosisSafeProxyFactoryInstance
-    .connect(setup.roles.prime)
-    .createProxy(setup.gnosisSafe.address, "0x00");
-  const proxy_receit = await proxy_tx.wait();
-  const proxy_addr = proxy_receit.events.filter((data) => {
-    return data.event === PROXY_CREATION;
-  })[0].args["proxy"];
-  return await ethers.getContractAt("GnosisSafe", proxy_addr);
-};
-
 const getContractInstance = async (factoryName, address, args) => {
   const Factory = await ethers.getContractFactory(factoryName, address);
   const parameters = args ? args : [];
@@ -27,6 +7,5 @@ const getContractInstance = async (factoryName, address, args) => {
 };
 
 module.exports = {
-  getGnosisProxyInstance,
   getContractInstance,
 };
